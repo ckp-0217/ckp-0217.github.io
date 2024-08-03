f658f4b8ef8eee1950c17b0883cabc334f3cbaa4
ckp-0217 <ckp0217@foxmail.com>
2024-08-03 23:09:27 +0800

    update from gridea: 2024-08-03 23:09:27

---
+++ media/scripts/common.js
@@ -0,0 +1,13 @@
+/**
+ * highlight.js Solidity syntax highlighting definition
+ *
+ * @see https://github.com/isagalaev/highlight.js
+ *
+ * @package: highlightjs-solidity
+ * @author:  Sam Pospischil <sam@changegiving.com>
+ * @since:   2016-07-01
+ */
+
+
+
+
